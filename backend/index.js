f6acf04add3be33972ff9f09b833301fdb2825dc
Niklas Enberg <nickeen95@gmail.com>
2022-12-07 13:59:44 +0100

    Implemented external backend

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ import fetch from 'node-fetch'
 
 const app = express()
 const port = 3000
-const beacons = {"monalisa": "57727444edc2cb3880cb7bf6"};
+const beacons = {"C58C6FC8479A419FA040EE34575CAD04": "57727444edc2cb3880cb7bf6", //Niklas Iphone, Mona Lisa
+    "24AB8B4EFD8C4E45AC79DFF20EF814A6":"577271f9edc2cb3880c37dcd", //Linus Iphone, The School of Athens
+    "C8232AFA1B79451BAD2ABB716704A8BF":"57726e2fedc2cb3880b61c00" //Toras Ipad, Composition A
+};
 
 app.get('/painting', (req, res) => {
     const id = beacons[req.query.id];

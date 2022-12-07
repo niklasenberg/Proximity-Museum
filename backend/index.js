81637f7583ac14abe7d2ef07596dc5ef2fb0ed73
Niklas Enberg <nickeen95@gmail.com>
2022-12-07 12:59:29 +0100

    Implemented external backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,23 @@
-const express = require('express')
+import express from 'express'
+import fetch from 'node-fetch'
+
 const app = express()
 const port = 3000
+const beacons = {"monalisa": "57727444edc2cb3880cb7bf6"};
+
+app.get('/painting', (req, res) => {
+    const id = beacons[req.query.id];
+    let url = "https://www.wikiart.org/en/api/2/Painting?id=" + id;
+    let settings = {method: "Get"};
+    fetch(url, settings)
+        .then(res => res.json())
+        .then((json) => {
+            res.json(json)
+        });
+})
 
 app.get('/', (req, res) => {
-    var id = req.query.id
-    res.send('Api is running')
+    res.send("Hello world!")
 })
 
 app.listen(port, () => {

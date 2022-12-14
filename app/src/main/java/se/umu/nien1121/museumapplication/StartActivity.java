5a3c318fafe91f8783c4036d231c687ef4856ae5
linusvallgren <linusvallgren@gmail.com>
2022-12-14 14:04:47 +0100

    Doing StartActivity Class

---
+++ app/src/main/java/se/umu/nien1121/museumapplication/StartActivity.java
@@ -0,0 +1,32 @@
+package se.umu.nien1121.museumapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import se.umu.nien1121.museumapplication.databinding.ActivityMainBinding;
+import se.umu.nien1121.museumapplication.databinding.ActivityStartBinding;
+
+public class StartActivity extends AppCompatActivity {
+    private ActivityStartBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityStartBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.scanBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                binding.scanBtn.setEnabled(false);
+                binding.scanBtn.setBackgroundColor(getResources().getColor(R.color.grey));
+                Intent resultIntent = new Intent(StartActivity.this,MainActivity.class);
+                startActivity(resultIntent);
+            }
+        });
+    }
+}

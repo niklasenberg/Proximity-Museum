356c3616f5743f586e47895ab26ad0199c8ff6b8
torawh <tora.hammarstrom@hotmail.com>
2022-12-30 18:20:01 +0100

    cleaned up code for ArtworkActivity

--- app/src/main/java/se/umu/nien1121/museumapplication/ArtworkActivity.java
+++ app/src/main/java/se/umu/nien1121/museumapplication/ArtworkActivity.java
@@ -1,17 +1,11 @@
 package se.umu.nien1121.museumapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
-
 import se.umu.nien1121.museumapplication.databinding.ActivityArtworkBinding;
 import se.umu.nien1121.museumapplication.model.Artwork;
 import se.umu.nien1121.museumapplication.utils.ActionBarHelper;
@@ -36,52 +30,54 @@ public class ArtworkActivity extends AppCompatActivity {
         binding.artworkNameText.setText(artwork.getTitle());
         binding.artworkArtistText.setText(artwork.getArtistName());
         binding.artworksFromArtistText.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
-        binding.artworksFromArtistText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent artistIntent = new Intent(ArtworkActivity.this, ArtistActivity.class);
-                artistIntent.putExtra(ArtistActivity.ARTIST_NAME_EXTRA, artwork.getArtistName());
-                artistIntent.putExtra(ArtistActivity.ARTIST_ID_EXTRA, artwork.getArtistId());
-                startActivity(artistIntent);
-            }
+        binding.artworksFromArtistText.setOnClickListener(view -> {
+            Intent artistIntent = new Intent(ArtworkActivity.this, ArtistActivity.class);
+            artistIntent.putExtra(ArtistActivity.ARTIST_NAME_EXTRA, artwork.getArtistName());
+            artistIntent.putExtra(ArtistActivity.ARTIST_ID_EXTRA, artwork.getArtistId());
+            startActivity(artistIntent);
         });
         setDescription();
+        setCompletionYear();
+        setImage();
+        makeImageClickable();
+    }
 
+    private void setImage() {
         DownloadImageTask imageTask = new DownloadImageTask(binding.imageView);
         imageTask.execute(artwork.getImage());
+    }
 
-        binding.imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    private void makeImageClickable() {
+        binding.imageView.setOnClickListener(view -> {
 
-                //View the image in full screen
-                AlertDialog.Builder alert = new AlertDialog.Builder(ArtworkActivity.this);
-                ImageView image = new ImageView(ArtworkActivity.this);
-                alert.setView(image);
-                image.setAdjustViewBounds(true);
-                DownloadImageTask imageTask2 = new DownloadImageTask(image);
-                imageTask2.execute(artwork.getImage());
-                alert.show();
+            //View the image in full screen
+            AlertDialog.Builder alert = new AlertDialog.Builder(ArtworkActivity.this);
+            ImageView image = new ImageView(ArtworkActivity.this);
+            alert.setView(image);
+            image.setAdjustViewBounds(true);
+            DownloadImageTask imageTask2 = new DownloadImageTask(image);
+            imageTask2.execute(artwork.getImage());
+            alert.show();
 
-            }
         });
+    }
 
-
+    private void setCompletionYear() {
         if (artwork.getCompletitionYear() != 0) {
-            binding.artworkCompletionYearText.setText(String.valueOf("Completion year: " + artwork.getCompletitionYear()));
+            binding.artworkCompletionYearText.setText("Completion year: " + artwork.getCompletitionYear());
         } else {
-            binding.artworkCompletionYearText.setText("Year unknown");
+            binding.artworkCompletionYearText.setText(R.string.unknown_year);
         }
     }
 
     private void setDescription() {
-        if (artwork.getDescription() != null && !artwork.getDescription().isEmpty()) {
+        if (artwork.getDescription() != null && !artwork.getDescription().isEmpty()){
 
             //removes everything within the brackets.
             String description = artwork.getDescription().replaceAll("\\[.*?\\]","");
             binding.artworkInfoText.setText(description);
         } else {
-            binding.artworkInfoText.setText("There is no description for this artwork.");
+            binding.artworkInfoText.setText(R.string.description_not_found);
         }
     }
 }

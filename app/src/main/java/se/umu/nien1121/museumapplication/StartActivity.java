fafb7ab21b80d5d621b58be0bdc9032c2cb88695
torawh <tora.hammarstrom@hotmail.com>
2022-12-30 18:30:29 +0100

    cleaned up code for StartActivity and string resources

--- app/src/main/java/se/umu/nien1121/museumapplication/StartActivity.java
+++ app/src/main/java/se/umu/nien1121/museumapplication/StartActivity.java
@@ -4,10 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
-import se.umu.nien1121.museumapplication.databinding.ActivityMainBinding;
 import se.umu.nien1121.museumapplication.databinding.ActivityStartBinding;
 import se.umu.nien1121.museumapplication.utils.ActionBarHelper;
 
@@ -19,17 +15,13 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityStartBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        ActionBarHelper.setActionBar(this, "Homepage");
+        ActionBarHelper.setActionBar(this, getResources().getString(R.string.actionbar_homepage));
 
-        binding.scanBtn.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                binding.scanBtn.setEnabled(false);
-                binding.scanBtn.setBackgroundColor(getResources().getColor(R.color.grey));
-                Intent resultIntent = new Intent(StartActivity.this,MainActivity.class);
-                startActivity(resultIntent);
-            }
+        binding.scanBtn.setOnClickListener(view -> {
+            binding.scanBtn.setEnabled(false);
+            binding.scanBtn.setBackgroundColor(getResources().getColor(R.color.grey));
+            Intent resultIntent = new Intent(StartActivity.this,MainActivity.class);
+            startActivity(resultIntent);
         });
     }
 
